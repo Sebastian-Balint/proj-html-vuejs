f44c0150f2e03f7674645c2086c922b0376f6f5a
Sebastian-Balint <sebastian.balint02@gmail.com>
2024-04-22 15:09:45 +0200

    Add header and footer base layout

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,6 +7,23 @@ export default {
 };
 </script>
 
-<template></template>
+<template>
+  <header>
+    <div class="debug container">
+      <nav class="debug flex">
+        <div class="debug logo"></div>
+        <div class="debug icon"></div>
+      </nav>
+      <div class="debug jumbotron flex">
+        <section class="debug">
+          <h1 class="serif">Barber Shop</h1>
+          <hr />
+          <p>The Pinnacle of Male Grooming</p>
+          <button>Learn More</button>
+        </section>
+      </div>
+    </div>
+  </header>
+</template>
 
 <style></style>
